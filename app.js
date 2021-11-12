aa59ebe48801aa890ff3e8be14dcfd1907624e60
maurocordoba2021 <echeverriamauro2017@gmail.com>
2021-11-11 22:21:20 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const { homedir } = require('os');
+const path=require('path')
+const app=express();
+
+app.use(express.static('public'));
+
+app.listen(3000,()=>{
+    console.log("levante el sv")
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})  
+
+app.get("/register.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"
+        ))
+})
+
+app.get("/login.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"
+        ))
+})
